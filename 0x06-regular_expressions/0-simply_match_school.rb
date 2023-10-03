936d2216b2166f47852699a8315916c432c042db
olisajioke <chijioke914@mail.com>
2023-10-03 13:28:06 -0700

    TASK 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
-require 'oniguruma'
 
-regex = Oniguruma::ORegexp.new('School')
-if regex.match(ARGV[0])
-  puts ARGV[0]
-else
-  puts "$"
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <input_string>"
+  exit(1)
 end
+
+pattern = /School/
+
+input_string = ARGV[0]
+matches = input_string.scan(pattern)
+
+result = matches.join
+
+puts result
