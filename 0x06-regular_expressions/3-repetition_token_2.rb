4086f055173110490d9d5805d5674c83c6664cad
olisajioke <chijioke914@mail.com>
2023-10-03 14:12:10 -0700

    task 3

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#Ruby code that prints htn and htbn
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <input_string>"
+  exit(1)
+end
+
+pattern = /hb+t+n/
+input_string = ARGV[0]
+
+match = pattern.match(input_string)
+
+if match
+  puts match[0]
+else
+  puts ''
+end
