3f529f914cebde84608062a92464d9e0399c2266
olisajioke <chijioke914@gmail.com>
2023-11-24 08:13:11 -0800

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Module: Create a file in /tmp/school with specific permissions
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
