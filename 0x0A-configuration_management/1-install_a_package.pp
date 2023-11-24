b82e5afe525cae3537a23c452e32747ae7da964d
olisajioke <chijioke914@gmail.com>
2023-11-24 13:53:16 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Module: Install Flask version 2.1.0 using pip3
+# Puppet manifest to install Flask version 2.1.0 using pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
