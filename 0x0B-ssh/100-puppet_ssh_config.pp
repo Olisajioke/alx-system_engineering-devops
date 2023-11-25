2b0f149490d309c2320212009c98acb180ec48e4
olisajioke <chijioke914@gmail.com>
2023-11-25 10:25:24 -0800

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env puppet
+# Puppet module to configure SSH client settings
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config', # Path to the SSH server config file
+  line   => 'PasswordAuthentication no', # Set PasswordAuthentication to no
+  match  => '^#?PasswordAuthentication', # Match the line regardless of comments
+}
+
+file_line { 'Declare identity file':
+  path   => '/home/your_username/.ssh/config', # Path to the SSH client config file
+  line   => 'IdentityFile ~/.ssh/school', # Set the identity file
+  match  => '^#?IdentityFile', # Match the line regardless of comments
+}
