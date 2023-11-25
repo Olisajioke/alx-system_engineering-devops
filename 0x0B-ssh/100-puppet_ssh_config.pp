338cbf747f84f70639a2033f9d171c96012166ba
olisajioke <chijioke914@gmail.com>
2023-11-25 11:52:07 -0800

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,7 +6,7 @@ file {'/etc/ssh/ssh_config':
 }
 
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^#PasswordAuthentication',
 }
