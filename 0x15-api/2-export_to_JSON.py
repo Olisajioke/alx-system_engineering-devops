e2320b94f2d7b6d850d7e4fdbc9af654bfaf3ea1
Olisajioke <chijioke914@gmail.com>
2024-01-02 11:32:55 -0800

    updating task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Python script to retrieve data from an API and convert to JSON"""
-
+""" A Python script that  gets data from an API and convert to Json"""
 import csv
 import json
 import requests
 import sys
 
-if __name__ == '__main__':
-    user_id = sys.argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/' + user_id
-    my_request = requests.get(user_url)
-    """Documentation"""
-    username = my_request.json().get('username')
-    """Documentation"""
-    task_url = user_url + '/todos'
-    my_request = requests.get(task_url)
-    my_tasks = my_request.json()
 
-    dict_file = {user_id: []}
-    for task in my_tasks:
-        completed_status = task.get('completed')
-        task_title = task.get('title')
-        dict_file[user_id].append({
-            "task": task_title,
-            "completed": completed_status,
-            "username": username
-        })
-    """print(dict_file)"""
-    with open('{}.json'.format(user_id), 'w') as file_1:
+if __name__ == '__main__':
+    USER_ID = sys.argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    response = requests.get(user_url)
+    USERNAME = response.json().get('username')
+    task_url = user_url + '/todos'
+    response = requests.get(task_url)
+    tasks = response.json()
+
+    dict_file = {USER_ID: []}
+    for task in tasks:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        dict_file[USER_ID].append({
+                                  "task": TASK_TITLE,
+                                  "completed": TASK_COMPLETED_STATUS,
+                                  "username": USERNAME})
+
+    with open('{}.json'.format(USER_ID), 'w') as file_1:
         json.dump(dict_file, file_1)
