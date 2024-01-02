15f59b9d0d514107d02bce2dd60c8c13a50a1b8c
Olisajioke <chijioke914@gmail.com>
2024-01-02 11:24:19 -0800

    updating task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""A python script that exports API data to CSV"""
-
+""" A python script that exports api to csv"""
 import csv
 import requests
 import sys
 
 if __name__ == '__main__':
     user = sys.argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-    my_requests = requests.get(url_user)
-    user = my_requests.json().get('username')
-    task = url_user + '/todos'
-    my_requests = requests.get(task)
-    my_tasks = my_requests.json()
+    url = 'https://jsonplaceholder.typicode.com/users/' + user
+    response = requests.get(url)
+    user_name = response.json().get('username')
+    task = url + '/todos'
+    response = requests.get(task)
+    tasks = response.json()
 
-    with open('{}.csv'.format(user), 'w') as my_csv:
-        for task in my_tasks:
+    with open('{}.csv'.format(user), 'w') as myfile:
+        for task in tasks:
             completed = task.get('completed')
             title_task = task.get('title')
-            my_csv.write('"{}","{}","{}","{}"\n'.format(
-                user, user, completed, title_task))
+            myfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
